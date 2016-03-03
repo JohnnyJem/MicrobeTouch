6629558bd5c71cb3a9f526ec4cb084e69f8986df
Johnny <trizelda@gmail.com>
2016-03-02 21:10:37 -0800

    Added p5 base capability with index.html and sketch.js template.

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+
+function setup() {
+      createCanvas(720, 480);
+      colorMode(HSB, 360, 100, 100);  //Mode : Hue/saturation/luminance
+      stroke(3);
+      fill(150);
+
+}
+
+
+function draw() {
+    frameRate(30);
+    //THE ORDER WE DRAW = THE ORDER THINGS ARE ORDERED/SET ON THE Z AXIS.
+
+}
+
+    function mouseClicked() {
+
+    }
+
+
+
+
+
+
